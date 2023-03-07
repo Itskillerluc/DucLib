9386e63f90a15bcaca78077f4c2630c81eecde77
Itskillerluc <86008039+Itskillerluc@users.noreply.github.com>
2023-03-07 16:10:11 +0100

    update latest (#1)
    * a few fixes
    
    * a few fixes

--- src/main/java/io/github/itskillerluc/duclib/DucLib.java
+++ src/main/java/io/github/itskillerluc/duclib/DucLib.java
@@ -2,21 +2,17 @@ package io.github.itskillerluc.duclib;
 
 import io.github.itskillerluc.duclib.data.animation.DucLibAnimationLoader;
 import io.github.itskillerluc.duclib.data.model.DucLibModelLoader;
-import com.mojang.logging.LogUtils;
 import net.minecraftforge.client.event.RegisterClientReloadListenersEvent;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
-import org.slf4j.Logger;
-
+//TODO: somehow make some kind of interpolation for when you have multiple animations running at once.
 @Mod(DucLib.MOD_ID)
 public class DucLib
 {
     public static final String MOD_ID = "duclib";
-    // Directly reference a slf4j logger
-    private static final Logger LOGGER = LogUtils.getLogger();
     public DucLib()
     {
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
